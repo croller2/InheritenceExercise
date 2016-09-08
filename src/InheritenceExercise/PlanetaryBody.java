b57d9f11fd584fdfb853895ec799a486f743cc18
croller2 <croller2@my.wctc.edu>
2016-09-08 08:50:02 -0500

    Initialized Git

---
+++ src/InheritenceExercise/PlanetaryBody.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package InheritenceExercise;
+
+/**
+ *
+ * @author chris
+ */
+public class PlanetaryBody extends CelestialBody {
+    private int yearLength;
+    private int numOfMoons;
+    private boolean hasRings;
+
+    public int getYearLength() {
+        return yearLength;
+    }
+
+    public void setYearLength(int yearLength) {
+        this.yearLength = yearLength;
+    }
+
+    public int getNumOfMoons() {
+        return numOfMoons;
+    }
+
+    public void setNumOfMoons(int numOfMoons) {
+        this.numOfMoons = numOfMoons;
+    }
+
+    public boolean isHasRings() {
+        return hasRings;
+    }
+
+    public void setHasRings(boolean hasRings) {
+        this.hasRings = hasRings;
+    }
+    
+    
+}
